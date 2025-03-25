ae8489b4cf19f8963e19755b573392e27c7be2b9
codin <douglasd3v.contato@gmail.com>
2025-03-25 17:49:49 -0300

    Add: Rota get para pegar token via header do usuário

--- backend/src/routes/UserRoutes.mjs
+++ backend/src/routes/UserRoutes.mjs
@@ -5,5 +5,6 @@ const router = express.Router();
 // Routes
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/checkuser', userController.checkUserByToken);
 
 export default router;
