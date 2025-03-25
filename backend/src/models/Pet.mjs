7fdcd791f5b96df91b2b37c95400048ec3515aef
codin <douglasd3v.contato@gmail.com>
2025-03-25 09:54:11 -0300

    Fix: Erro no caminho para conexão com o banco de dados

--- backend/src/models/Pet.mjs
+++ backend/src/models/Pet.mjs
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
-import connection from '../db/connection.mjs';
+import connection from '../../db/connection.mjs';
 
 const Pet = mongoose.model(
   'Pet',
