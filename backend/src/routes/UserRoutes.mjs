3c43bca1088ccea59ac436458af5a141ff1f5e5d
codin <douglasd3v.contato@gmail.com>
2025-03-25 19:43:16 -0300

    Add: Rota get para puxar o usuário pelo ID.

--- backend/src/routes/UserRoutes.mjs
+++ backend/src/routes/UserRoutes.mjs
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/checkuser', userController.checkUserByToken);
+router.get('/:id', userController.getUserById);
 
 export default router;
