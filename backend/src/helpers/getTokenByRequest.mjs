3a99d957f17e3f829534e40422b2472906e0044b
codin <douglasd3v.contato@gmail.com>
2025-03-25 18:08:18 -0300

    Add: Função para buscar o token pela header

---
+++ backend/src/helpers/getTokenByRequest.mjs
@@ -0,0 +1,8 @@
+function getTokenByRequest(req) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(' ')[1];
+
+  return token;
+}
+
+export default getTokenByRequest;
