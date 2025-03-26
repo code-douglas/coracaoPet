7898a6b11b3df775ab6934e2449f1cd55242db53
codin <douglasd3v.contato@gmail.com>
2025-03-26 09:28:20 -0300

    Fix: Bug que retornava undefined no token removido

--- backend/src/helpers/getTokenByRequest.mjs
+++ backend/src/helpers/getTokenByRequest.mjs
@@ -1,6 +1,9 @@
 function getTokenByRequest(req) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader.split(' ')[1];
+  const authHeader = req.headers?.authorization;
+  if (!authHeader) return null;
+
+  const tokenParts = authHeader.split(' ');
+  const token = tokenParts.length > 1 ? tokenParts[tokenParts.length - 1] : null;
 
   return token;
 }
