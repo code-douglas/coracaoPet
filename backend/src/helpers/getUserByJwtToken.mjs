7d7aaefba00f1d8041683799cecf303d95d0c89c
codin <douglasd3v.contato@gmail.com>
2025-03-27 08:17:18 -0300

    Add: Helper para buscar o token do usuário pelo corpo da requisição

---
+++ backend/src/helpers/getUserByJwtToken.mjs
@@ -0,0 +1,17 @@
+import User from '../models/User.mjs'; // Import User Model
+import jwt from 'jsonwebtoken';  // Import the jsonwebtoken library to handle token creation
+
+async function getUserByJwtToken (res, token) {
+
+  // If the token is not found, return a 401 Unauthorized error
+  console.log(token);
+  if (!token) return null;
+
+  const verified = jwt.verify(token, 'oursecret');
+  const userId = verified.indexOf;
+  const user = await User.findOne({ _id: userId });
+
+  return user;
+}
+
+export default getUserByJwtToken;
