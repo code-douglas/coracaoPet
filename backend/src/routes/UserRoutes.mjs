e32433abb91f202a09b4a60aba5a6cfdab97987a
codin <douglasd3v.contato@gmail.com>
2025-04-03 11:37:50 -0300

    Remove: Comentários

--- backend/src/routes/UserRoutes.mjs
+++ backend/src/routes/UserRoutes.mjs
@@ -1,6 +1,7 @@
 import express from 'express';  // Import Express to create a router and handle HTTP requests
 import userController from '../controllers/UserController.mjs';  // Import the UserController that handles business logic for user routes
 import checkUserToken from '../middlewares/checkUserToken.mjs';  // Import middleware to check if the user is authenticated via token
+import imagemUpload from '../helpers/imagemUpload.mjs'; // Import Helper for upload image in db.
 
 // Create a new router instance using Express
 const router = express.Router();
@@ -24,7 +25,7 @@ router.get('/:id', userController.getUserById);
 // GET request to '/:id' will retrieve user details using the 'getUserById' function in the UserController. The ':id' is a dynamic parameter.
 
 // Edit user information (only authenticated users)
-router.patch('/edit/:id', checkUserToken, userController.editUser);
+router.patch('/edit/:id', checkUserToken, imagemUpload.single('image'), userController.editUser);
 // PATCH request to '/edit/:id' will trigger the 'editUser' function in the UserController. The 'checkUserToken' middleware ensures that the request is authenticated before proceeding.
 
 export default router;
