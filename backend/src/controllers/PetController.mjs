c2308408f4396a99a6c165002edb75fddcbcdf49
codin <douglasd3v.contato@gmail.com>
2025-04-07 09:33:13 -0300

    Add: Validação para campo de envio de imagens, se tornando obrigatório

--- backend/src/controllers/PetController.mjs
+++ backend/src/controllers/PetController.mjs
@@ -6,11 +6,13 @@ import getUserByJwtToken from '../helpers/getUserByJwtToken.mjs';
 class PetController {
   static async create(req, res) {
     const { name, age, weight, color } = req.body;
+    const images = req.files;
     const available = true;
     let contract = new ValidationContract();
 
     // Validation
     contract.isRequired(name, 'O campo nome é obrigatório.');
+    contract.isRequired(images, 'A imagem é obrigatório.');
     contract.hasMinLen(name, 3, 'O campo nome, deve conter no minimo 3 caracteres.');
     contract.isRequired(age, 'O campo idade é obrigatório.');
     contract.isRequired(weight, 'O campo peso é obrigatório.');
@@ -36,6 +38,10 @@ class PetController {
         image: user.image,
         phone: user.phone
       }
+    });
+
+    images.map((image) => {
+      pet.images.push(image.filename);
     });
 
     try {
