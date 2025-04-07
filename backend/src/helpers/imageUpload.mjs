7dbbf58c1303bffa2c1e8d375d6c36a67319972c
codin <douglasd3v.contato@gmail.com>
2025-04-07 09:32:33 -0300

    Alt: forma que o multer irá salvar os nomes das imagens com o math floor/random

---
+++ backend/src/helpers/imageUpload.mjs
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import path from 'path';
+
+const imageStorage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    let folder = '';
+
+    if(req.baseUrl.includes('users')) {
+      folder = 'users';
+    } else if(req.baseUrl.includes('pets')) {
+      folder = 'pets';
+    }
+
+    cb(null, `src/public/images/${folder}`);
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + String(Math.floor(Math.random() * 10)) + path.extname(file.originalname));
+  }
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+      return cb(new Error('Envie apenas, JPEG, PNG ou JPG.'));
+    }
+    cb(undefined, true);
+  }
+});
+
+export default imageUpload;
