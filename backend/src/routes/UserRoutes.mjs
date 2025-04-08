42bf9a6a999c62b792fad79914d704cc56a60b8b
codin <douglasd3v.contato@gmail.com>
2025-04-07 21:40:29 -0300

    Fix: Importação do helper de imagens

--- backend/src/routes/UserRoutes.mjs
+++ backend/src/routes/UserRoutes.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import userController from '../controllers/UserController.mjs';
 import checkUserToken from '../middlewares/checkUserToken.mjs';
-import imagemUpload from '../helpers/imagemUpload.mjs';
+import imageUpload from '../helpers/imageUpload.mjs';
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/checkuser', userController.checkUserByToken);
 router.get('/:id', userController.getUserById);
-router.patch('/edit/:id', checkUserToken, imagemUpload.single('image'), userController.editUser);
+router.patch('/edit/:id', checkUserToken, imageUpload.single('image'), userController.editUser);
 
 export default router;
