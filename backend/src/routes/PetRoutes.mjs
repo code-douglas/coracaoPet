e1681441ea8c668a2e52777749a9c9cb060ec399
codin <douglasd3v.contato@gmail.com>
2025-04-09 09:26:42 -0300

    Add: Rota para remoção de pets

--- backend/src/routes/PetRoutes.mjs
+++ backend/src/routes/PetRoutes.mjs
@@ -10,5 +10,6 @@ router.get('/:id', PetController.getPetById);
 router.get('/mypets', checkUserToken, PetController.getAllUserPet);
 router.get('/myadoptions', checkUserToken, PetController.getAllUserAdoptions);
 router.post('/create', checkUserToken, imageUpload.array('images'), PetController.create);
+router.delete('/:id', checkUserToken, PetController.removePetById);
 
 export default router;
