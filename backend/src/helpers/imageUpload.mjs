17628fe6ba8bb3a34a4a7897ef355ec5907a6c87
codin <douglasd3v.contato@gmail.com>
2025-04-10 08:55:45 -0300

    Remove: imagens da pasta public, alt: local da pasta

--- backend/src/helpers/imageUpload.mjs
+++ backend/src/helpers/imageUpload.mjs
@@ -11,7 +11,7 @@ const imageStorage = multer.diskStorage({
       folder = 'pets';
     }
 
-    cb(null, `src/public/images/${folder}`);
+    cb(null, `/public/images/${folder}`);
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + String(Math.floor(Math.random() * 10)) + path.extname(file.originalname));
