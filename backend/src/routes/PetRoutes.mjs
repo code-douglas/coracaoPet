196bdc88fed2196ae51c41def74e25f8895fb4b8
codin <douglasd3v.contato@gmail.com>
2025-04-10 09:14:45 -0300

    Fix: erro no chamada da função e no field de image

--- backend/src/routes/PetRoutes.mjs
+++ backend/src/routes/PetRoutes.mjs
@@ -9,7 +9,7 @@ router.get('/', PetController.getAllPets);
 router.get('/:id', PetController.getPetById);
 router.get('/mypets', checkUserToken, PetController.getAllUserPet);
 router.get('/myadoptions', checkUserToken, PetController.getAllUserAdoptions);
-router.post('/create', checkUserToken, imageUpload.array('images'), PetController.create);
+router.post('/create', checkUserToken, imageUpload.array('image'), PetController.create);
 router.delete('/:id', checkUserToken, PetController.removePetById);
 
 export default router;
