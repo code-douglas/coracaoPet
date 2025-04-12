6c8765ad5753902cc88531b5d8e9b4a15c9825c5
codin <douglasd3v.contato@gmail.com>
2025-04-12 14:32:39 -0300

    Alt: status codes mais condizentes as solicitaçoes

--- backend/src/controllers/UserController.mjs
+++ backend/src/controllers/UserController.mjs
@@ -24,24 +24,23 @@ class UserController {
     }
 
     if (!contract.isValid()) {
-      return res.status(422).json({ errors: contract.errors });
+      return res.status(400).json({ errors: contract.errors });
     }
 
     const checkUserExist = await User.findOne({ email });
     if (checkUserExist) {
-      return res.status(422).json({ message: 'Usuário já cadastrado.' });
+      return res.status(409).json({ message: 'Usuário já cadastrado.' });
     }
 
     const salt = await bcrypt.genSalt(12);
     const passwordHash = await bcrypt.hash(password, salt);
-
     const user = new User({ name, email, phone, password: passwordHash });
 
     try {
       const newUser = await user.save();
       await createUserToken(req, res, newUser);
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao criar usuário, tente novamente.', error });
+      res.status(503).json({ message: 'Erro ao criar usuário, tente novamente.', error });
     }
   }
 
@@ -53,23 +52,23 @@ class UserController {
     contract.isRequired(password, 'O campo senha é obrigatória.');
 
     if (!contract.isValid()) {
-      return res.status(422).json({ errors: contract.errors });
+      return res.status(400).json({ errors: contract.errors });
     }
 
     try {
       const user = await User.findOne({ email });
       if (!user) {
-        return res.status(422).json({ message: 'Usuário não cadastrado.' });
+        return res.status(404).json({ message: 'Usuário não cadastrado.' });
       }
 
       const checkPassword = await bcrypt.compare(password, user.password);
       if (!checkPassword) {
-        return res.status(422).json({ message: 'Verifique suas informações e tente novamente.' });
+        return res.status(401).json({ message: 'Verifique suas informações e tente novamente.' });
       }
 
       await createUserToken(req, res, user);
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao realizar login, tente novamente.', error });
+      res.status(503).json({ message: 'Erro ao realizar login, tente novamente.', error });
     }
   }
 
@@ -87,7 +86,6 @@ class UserController {
 
       return res.status(200).json(currentUser);
     } catch (error) {
-      console.error('Erro ao verificar usuário:', error);
       return res.status(401).json({ message: 'Token inválido ou expirado.' });
     }
   }
@@ -104,7 +102,7 @@ class UserController {
 
       res.status(200).json({ user });
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao buscar usuário, tente novamente.', error });
+      res.status(503).json({ message: 'Erro ao buscar usuário, tente novamente.', error });
     }
   }
 
@@ -147,14 +145,14 @@ class UserController {
     }
 
     if (!contract.isValid()) {
-      return res.status(422).json({ errors: contract.errors });
+      return res.status(400).json({ errors: contract.errors });
     }
 
     try {
       await userToUpdate.save();
       res.status(200).json({ message: 'Usuário atualizado com sucesso.' });
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao atualizar usuário, tente novamente.', error });
+      res.status(503).json({ message: 'Erro ao atualizar usuário, tente novamente.', error });
     }
   }
 }
